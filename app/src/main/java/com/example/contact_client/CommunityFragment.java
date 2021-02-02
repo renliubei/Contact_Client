d140b23d26bedfedd427b18082048c5b1d4ae84a
杨冬帽 <573082640@qq.com>
2021-02-02 15:53:15 +0800

    主导航

---
+++ app/src/main/java/com/example/contact_client/CommunityFragment.java
@@ -0,0 +1,36 @@
+package com.example.contact_client;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CommunityFragment extends Fragment {
+
+    private CommunityViewModel mViewModel;
+
+    public static CommunityFragment newInstance() {
+        return new CommunityFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.community_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CommunityViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/contact_client/FactoryFragment.java
@@ -0,0 +1,36 @@
+package com.example.contact_client;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FactoryFragment extends Fragment {
+
+    private FactoryViewModel mViewModel;
+
+    public static FactoryFragment newInstance() {
+        return new FactoryFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.factory_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(FactoryViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
