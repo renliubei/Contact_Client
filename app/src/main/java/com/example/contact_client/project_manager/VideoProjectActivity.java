b18bd0f2b040115949d585a48642cea22d7ce16c
杨冬帽 <573082640@qq.com>
2021-03-05 00:03:35 +0800

    修改：移动text到下方

--- app/src/main/java/com/example/contact_client/project_manager/VideoProjectActivity.java
+++ app/src/main/java/com/example/contact_client/project_manager/VideoProjectActivity.java
@@ -2,12 +2,15 @@ package com.example.contact_client.project_manager;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import com.example.contact_client.R;
 import com.example.contact_client.databinding.ActivityVideoProjectBinding;
@@ -18,7 +21,7 @@ import java.util.List;
 public class VideoProjectActivity extends AppCompatActivity {
     ProjectViewModel mViewModel;
     ActivityVideoProjectBinding binding;
-    ProjectGalleryAdapter adapter;
+    GalleryAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,17 +37,33 @@ public class VideoProjectActivity extends AppCompatActivity {
         mViewModel.getProjectsLiveDataList().observe(this, new Observer<List<VideoProject>>() {
             @Override
             public void onChanged(List<VideoProject> videoProjects) {
-                adapter.setData(videoProjects);
+                adapter.setVideoProjects(videoProjects);
                 adapter.notifyDataSetChanged();
             }
         });
     }
 
     void modifyRecyclerView(){
-        adapter = new ProjectGalleryAdapter();
+        adapter = new GalleryAdapter();
         binding.recyclerViewProjects.setAdapter(adapter);
         binding.recyclerViewProjects.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
-        LinearSnapHelper snapHelper = new LinearSnapHelper();
+        SnapHelper snapHelper = new LinearSnapHelper();
         snapHelper.attachToRecyclerView(binding.recyclerViewProjects);
+        binding.recyclerViewProjects.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (recyclerView.getChildCount() > 0) {
+                    try {
+                        int currentPosition = ((RecyclerView.LayoutParams) recyclerView.getChildAt(0).getLayoutParams()).getViewAdapterPosition();
+                        VideoProject videoProject = adapter.getVideoProjects().get(currentPosition);
+                        binding.textViewProjectName.setText(videoProject.getName()+videoProject.getId());
+                        binding.textViewProjectDesc.setText(videoProject.getDescription());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
